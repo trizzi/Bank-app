2abdc1d8e9e512d9b2ed7029f8bf8df0b11781f2
Ric <ric@Doom-Machine.local>
2021-10-06 03:03:13 +0100

    Finished frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,61 @@
+import React, { Fragment } from 'react';
 import {
 	BrowserRouter as Router,
 	Route,
 	Switch,
 } from 'react-router-dom';
+import Navbar from './components/layouts/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
-import Navbar from './components/layouts/Navbar';
+import AuthState from './context/auth/AuthState';
+import Register from './context/auth/Register';
+import Login from './context/auth/Login';
+import AlertState from './context/alert/AlertState';
+import Alerts from './components/layouts/Alerts';
+import setAuthToken from './utils/setAuthToken';
+import PrivateRoute from './components/routing/PrivateRoute';
 
-function App() {
-	return (
-		<Router>
-			<>
-				<Navbar />
-				<div className='container'>
-					<Switch>
-						<Route exact path='/' component={Home} />
-						<Route exact path='/about' component={About} />
-					</Switch>
-				</div>
-			</>
-		</Router>
-	);
+if (localStorage.token) {
+	setAuthToken(localStorage.token);
 }
+
+const App = () => {
+	return (
+		<AuthState>
+			<AlertState>
+				<Router>
+					<Fragment>
+						<Navbar />
+						<div className='container'>
+							<Alerts />
+							<Switch>
+								<PrivateRoute
+									exact
+									path='/'
+									component={Home}
+								/>
+								<Route
+									exact
+									path='/about'
+									component={About}
+								/>
+								<Route
+									exact
+									path='/register'
+									component={Register}
+								/>
+								<Route
+									exact
+									path='/login'
+									component={Login}
+								/>
+							</Switch>
+						</div>
+					</Fragment>
+				</Router>
+			</AlertState>
+		</AuthState>
+	);
+};
 
 export default App;
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,7 +1,13 @@
 const Home = () => {
 	return (
 		<div>
-			<h1>Home</h1>
+			<h1>Welcome Message</h1>
+			<p className='my-1 bg-dark p'>
+				Hello folks. Thanks for joining us. Do bear with us
+				for this weird looking page. We will update it as
+				soon as we receive intructions to do so. Thanks and
+				Best Regards.
+			</p>
 		</div>
 	);
 };
